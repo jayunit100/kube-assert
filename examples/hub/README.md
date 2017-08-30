747d58b5bcb211a8a2d4a00da687d5578f937df2
Jay Vyas <jvyas@bds01004.bds-ad.lc>
2017-08-30 13:26:22 -0400

    make the hub a special case example, first attempt at refactoring

---
+++ examples/hub/README.md
@@ -0,0 +1,4 @@
+# This is a test for the blackduck hub app.  Its experimental.
+# All rights reserved, blackduck software inc.
+
+
--- kube-create.sh
+++ kube-create.sh
@@ -7,21 +7,33 @@ set -e
 NS="testing"
 NODENAME="kubernetes-minion-group-b2s6"
 
+## TODO Move these definitions into examples/ so this is kube generic.
+CREATE_FILE_1="kubernetes-pre-db.yml.template"
+CREATE_FILE_1_PODS="2"
+CREATE_FILE_2="kubernetes-post-db.yml.template"
+CREATE_FILE_2_PODS="8"
+
 kubectl create -f namespace.json
 kubectl create -f pods-testing.env --namespace=$NS
+
+## TODO Somehow make label operations kube generic.
 kubectl label --overwrite nodes $NODENAME blackduck.hub.postgres=true --namespace=$NS
 
 sudo rm -rf /var/lib/hub-postgresql
 sudo mkdir -p /var/lib/hub-postgresql/data 
 sudo chmod -R 775 /var/lib/hub-postgresql/data
 
-kubectl create -f kubernetes-pre-db.yml.template --namespace=$NS
-until [ $(kubectl get pods --namespace=$NS | awk '{ print $3 }' | grep Running | wc -l) -eq 2 ]; do
+
+# TODO Make this a for loop with N create files.
+kubectl create -f $CREATE_FILE_1 --namespace=$NS
+until [ $(kubectl get pods --namespace=$NS | awk '{ print $3 }' | grep Running |
+    wc -l) -eq $CREATE_FILE_1_PODS ]; do
 	sleep 2
 done
  
-kubectl create -f kubernetes-post-db.yml.template --namespace=$NS
-until [ $(kubectl get pods --namespace=$NS | awk '{ print $3 }' | grep Running | wc -l) -eq 8 ];
+kubectl create -f $CREATE_FILE_2 --namespace=$NS
+until [ $(kubectl get pods --namespace=$NS | awk '{ print $3 }' | grep Running |
+    wc -l) -eq $CREATE_FILE_2_PODS ];
 do
 	sleep 2
 done
@@ -31,4 +43,5 @@ sleep 1m
  
 sh ./kube-tests.sh
 
+# TODO comment why we comment this out :)
 # kubectl delete -f namespace.json
